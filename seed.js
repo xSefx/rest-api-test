7073b438f5a37ab25ff043bbd46d8f355b97675d
Dima <shetkodv@gmail.com>
2021-05-21 15:44:14 +0300

    add postman

--- seed.js
+++ seed.js
@@ -16,8 +16,8 @@ const createItem = async (name, description, token, file) => {
 
 const seed = async () => {
   try {
-    const user1 = await createUser('user121s3ssda@mail.ru', '132');
-    const user2 = await createUser('user1121awa33212s@mail.ru', '132');
+    const user1 = await createUser('user123@mail.ru', '132');
+    const user2 = await createUser('user2211qq@mail.ru', '132');
 
     await createItem('item1', 'desc item1', user1, '13213123.jpeg');
     await createItem('item2', 'desc item2', user2, '1559370578_1.jpg');
